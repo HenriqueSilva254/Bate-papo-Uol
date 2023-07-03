09df6da0bf16174fee6cfca01af45dd49c01654a
Henrique <henriquesilva254@gmail.com>
2023-07-02 23:57:22 -0300

    Back end BatepapoUol validacao joi messages

--- src/app.js
+++ src/app.js
@@ -60,8 +60,21 @@ app.get('/participants', (req, res) => {
 app.post('/messages', async (req, res) => {
   const { to, text, type } = req.body
   const { user } = req.headers
+  console.log(user)
+  const userSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required()
+  })
+  const validacao = userSchema.validate(req.body)
+  if (validacao.error || user === undefined) {
+    const errors = validacao.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
 
   try {
+    const promisse = await db.collection("participants").findOne({ name: user}) 
+    if(!promisse) return res.sendStatus(422)
     await db.collection("messages").insertOne({ from: user, to, text, type, time: dayjs().format('HH:mm:ss') })
     res.sendStatus(201)
   } catch (err) {
@@ -91,13 +104,16 @@ app.get('/messages', async (req, res) => {
   }
 });
 
-app.post('/status', async (req, res) => {
+app.put('/status', async (req, res) => {
   const { user } = req.headers
-  if(user) return res.sendStatus(404)
+  //const newUser = {name: user, }
+  //if(!user) return res.sendStatus(404)
   try {
-    const name = await db.collection("status").findOne({ user: user})
-    if (name) return  res.sendStatus(404)
-    res.sendStatus(201)
+    const promisse = await db.collection("participants").findOne({ name: user}) 
+    if(!promisse) return res.sendStatus(404)
+    await db.collection("participants").updateOne({ name: user}, {$set: {name: user, lastStatus: Date.now()}}) 
+    res.sendStatus(200)
+
   } catch (err) {
     res.sendStatus(404)
   }
